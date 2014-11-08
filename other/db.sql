a1f3ee802b72fa4f2d95669d00926d780806976d
Humphrey Shotton <humpheh@gmail.com>
2014-11-08 12:31:06 +0000

    Database Init and Config
    Added the init file and the config file for connecting to the database.

---
+++ other/db.sql
@@ -0,0 +1,35 @@
+CREATE TABLE user (
+    user_id SERIAL,
+    user_username VARCHAR(20) NOT NULL,
+    user_password VARCHAR(100) NOT NULL,
+    user_weight INT NULL,
+    user_gender ENUM('none', 'male', 'female')
+) ENGINE=INNODB;
+
+CREATE TABLE session (
+    session_id SERIAL,
+    session_user BIGINT UNSIGNED,
+    session_timestart TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    session_timefinish TIMESTAMP NULL DEFAULT NULL,
+
+    FOREIGN KEY (session_id)
+        REFERENCES user(user_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE drink (
+    drink_id SERIAL,
+    drink_name VARCHAR(100) NOT NULL,
+    drink_units FLOAT NOT NULL,
+    drink_volume INT NOT NULL,
+    drink_picture VARCHAR(200) NULL,
+    drink_calories INT
+) ENGINE=INNODB;
+
+CREATE TABLE sessiondrink (
+    sessdr_id SERIAL,
+    sessdr_session_id BIGINT UNSIGNED,
+    sessdr_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    sessdr_volume INT 
+) ENGINE=INNODB;

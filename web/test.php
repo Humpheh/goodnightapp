6a1e5f74766e538aeed58bdea5691690547b5196
Max Maybury <max@maxjmay.co.uk>
2014-11-08 23:20:59 +0000

    Testing one page scroll.

--- web/test.php
+++ web/test.php
@@ -2,11 +2,9 @@
 
 include 'init.php';
 
-?>
+require 'header.php';
 
-<?php require 'header.php'; ?>
-
-<?php if(Logins::isLoggedIn()){
+if(Logins::isLoggedIn()){
     include 'inc/session.php';
 } else { 
 
@@ -21,23 +19,61 @@ if (!empty($_POST['password']) && !empty($_POST['username'])){
         exit();
     }
 } ?>
+	<meta charset="utf-8">
 
 
-<div style="height:100%;width:100%;text-align:center;display:table">
-    <div style="display:table-cell;vertical-align:middle;">
-        <section id="homeSection">
-            <div class="container">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script type="text/javascript" src="js/jquery.onepage-scroll.js"></script>
+  <link href='cs/onepage-scroll.css' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="initial-scale=1, width=device-width, maximum-scale=1, minimum-scale=1, user-scalable=no">
+  <style>
+     html {
+      height: 100%;
+   }
+    body {
+      background: #E2E4E7;
+      padding: 0;
+      text-align: center;
+      font-family: 'open sans';
+      position: relative;
+      margin: 0;
+      height: 100%;
+      -webkit-font-smoothing: antialiased;
+    }
+    
+    .wrapper {
+    	height: 100% !important;
+    	height: 100%;
+    	margin: 0 auto; 
+    	overflow: hidden;
+    }
+  
+	</style>
+	<script>
+	  $(document).ready(function(){
+      $(".main").onepage_scroll({
+        sectionContainer: "section",
+        responsiveFallback: 600,
+        loop: true
+      });
+		});
+		
+	</script>
+</head>
+<body>
+  <div class="wrapper">
+	  <div class="main">
+	    
+      <section class="page1">
+        <div class="container">
                 <h1 style="font-size:100px;font-weight:bold;font-style:italic;color:white;">Great Night!</h1>
                 <span style="font-size:30px;"><a href="login.php">Login</a> - <a href="register.php">Register</a></span>
 
-                <a href="#" style="display:block;font-weight:normal;font-size:30px;width:100%;position:absolute;padding:15px 0;bottom:0px;">
-                    Next<br/>
-                    <span class="glyphicon glyphicon-chevron-down"></span>
-                </a>
             </div>
-        </section>
-        <section>
-            <div class="container">
+      </section>
+	    
+	    <section class="page2">
+	      <div class="container">
                 <h1>Login</h1>
 
                 <form method="POST" role="form">
@@ -50,14 +86,8 @@ if (!empty($_POST['password']) && !empty($_POST['username'])){
                     <input type="submit" name="login" value="Login" class="btn btn-block btn-primary"/>
                 </form>
             </div>
-        </section>
+      </section>
     </div>
-</div>
-<script type="text/javascript" src="js/onepagescroll/jquery.onepage-scroll.js"></script>
-
-You are not logged in. <a href="login.php">Login</a> - <a href="register.php">Register</a>
-<?php } ?>
+  </div>
 
 <?php require 'footer.php'; ?>
-
-
